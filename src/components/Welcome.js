c41407996481517bfa4ab6c252fa5bc5ff4c364e
RodriJOk <juarezrodrigo59@gmail.com>
2022-01-23 21:46:01 -0300

    Fix styles in Welcome component. Add URL in social section and Change message in home.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,28 +12,30 @@ const Welcome = () => {
                     <span>Bienvenido a mi website! 👋</span>
                 </i>
             </h1>
-            <div className="home_mini_container">
+            <div className="home__description">
+                <p>
+                    Hola! Me llamo Alan Rodrigo Juarez 👦🏻. Soy desarrollador Web Front-End,  👨‍💻 y me encanta codear en JS 💛.
+                    Soy una persona autodidacta, apasionado por la tecnologia 🦾 y el desarrollo web 💻, que le encanta compartir mis conocimientos con la comunidad 🤓.</p>
+            </div>
+
+            <div className='home__contactme'>
                 <div className="home__scroll">
-                    <a href="#about" className="home__scroll-link">
+                    <a href="#about">
                         Scroll down
                     </a>
                     <FontAwesomeIcon icon={faArrowDown} className='faArrowUp'/>
                 </div>
-                <div class="text-cursor__prov">
-                    <p>Hola! Me llamo Alan Rodrigo Juarez. Soy desarrollador Web Front-End y me encanta codear en JS</p>
-                </div>
-            </div>
-
-            <div className='social-section'>
-                    <a href="https://www.linkedin.com/in/alan-rodrigo-juarez-a8a8b5a4/" target="_blank" rel="noopener noreferrer">
+                <div className='home__social'>
+                    <a href="https://www.linkedin.com/in/rodrijuarez/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faLinkedin} className='faLinkedin'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/alan-rodrigo-juarez-a8a8b5a4/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/RodriJOk" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faGithub} className='faGithub'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/alan-rodrigo-juarez-a8a8b5a4/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.instagram.com/preguntastech" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faInstagram} className='faInstagram'/>
                     </a>
+                </div>
             </div>
         </section>
     );
